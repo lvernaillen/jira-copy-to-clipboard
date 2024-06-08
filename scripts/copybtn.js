bf62a042c62c662596a24a76bf424825fcbb89b6
Lieven Vernaillen <lieven.vernaillen@smartphoto.com>
2024-06-08 18:43:46 +0200

    Match Jira quick add buttons style

--- scripts/copybtn.js
+++ scripts/copybtn.js
@@ -56,22 +56,50 @@ function notify(message)
 }
 
 function createButton(parent) {
-  const buttonText = 'Jira Snippet';
-  const button = document.createElement("button");
-  button.textContent = buttonText;
-  button.id = "CopyBtnJiraId";
-  button.className = "CopyBtnForJira";
-  parent.appendChild(button);
+  const buttonIconSpan = document.createElement("span");
+  buttonIconSpan.classList.add("css-1uc6u2g")
+  buttonIconSpan.innerHTML = `
+    <span class="_ca0qidpf _u5f3idpf _n3tdidpf _19bvidpf _18u0r5cr _2hwx1i6y">
+      <span aria-hidden="true" class="css-1afrefi" style="--icon-primary-color: currentColor; --icon-secondary-color: var(--ds-surface, #FFFFFF);">
+        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="enable-background:new 0 0 24 24" xml:space="preserve">
+          <g transform="translate(3 3) scale(0.1465)">
+            <path d="M89.62,13.96v7.73h12.19h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02v0.02 v73.27v0.01h-0.02c-0.01,3.84-1.57,7.33-4.1,9.86c-2.51,2.5-5.98,4.06-9.82,4.07v0.02h-0.02h-61.7H40.1v-0.02 c-3.84-0.01-7.34-1.57-9.86-4.1c-2.5-2.51-4.06-5.98-4.07-9.82h-0.02v-0.02V92.51H13.96h-0.01v-0.02c-3.84-0.01-7.34-1.57-9.86-4.1 c-2.5-2.51-4.06-5.98-4.07-9.82H0v-0.02V13.96v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07V0h0.02h61.7 h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02V13.96L89.62,13.96z M79.04,21.69v-7.73v-0.02h0.02 c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v64.59v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h12.19V35.65 v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07v-0.02h0.02H79.04L79.04,21.69z M105.18,108.92V35.65v-0.02 h0.02c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v73.27v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h61.7h0.02 v0.02c0.91,0,1.75-0.39,2.37-1.01c0.61-0.61,1-1.46,1-2.37h-0.02V108.92L105.18,108.92z"
+            fill="currentColor" fill-rule="evenodd"/>
+          </g>
+        </svg>
+      </span>
+    </span>`
 
+  const buttonTextSpan = document.createElement("span");
+  buttonTextSpan.classList.add("css-178ag6o")
+  const buttonText = 'Copy to clipboard';
+  buttonTextSpan.textContent = buttonText
+
+  const button = document.createElement("button");
+  button.id = "copy-to-clipboard-button";
+  button.classList.add("css-1luyhz2")
+  button.appendChild(buttonIconSpan)
+  button.appendChild(buttonTextSpan)
+
+  const whiteSpaceSpan = document.createElement("span");
+  whiteSpaceSpan.classList.add("button-span", "_2hwxu2gc")
+  whiteSpaceSpan.appendChild(button)
+
+  const div = document.createElement("div");
+  div.setAttribute("role", "presentation");
+  div.appendChild(whiteSpaceSpan)
+
+  parent.appendChild(div);
+  
   button.onclick = function () {
         const issueId = getIssueId();
         if(issueId == null) {
-          button.textContent = 'Error: No Issue id found!';
+          buttonTextSpan.textContent = 'Error: No Issue id found!';
         }
         getIssueDataAndWriteToClipboard(issueId);
-        button.textContent = 'Text has been copied!';
+        buttonTextSpan.textContent = 'Text has been copied!';
         setTimeout(function () {
-          button.textContent = buttonText;
+          buttonTextSpan.textContent = buttonText;
         }, 2000);
   };
 }
@@ -81,7 +109,7 @@ var observer = new MutationObserver(function (mutations, me) {
                document.getElementsByClassName('_otyr1y44 _ca0q1y44 _u5f3idpf _n3td1y44 _19bvidpf _1e0c116y')[0] ??
                document.getElementsByClassName('_otyr1b66 _1yt4swc3 _1e0c116y')[0];
   if (parent) {
-    if (!document.getElementById('CopyBtnJiraId')) {
+    if (!document.getElementById('copy-to-clipboard-button')) {
       createButton(parent);
     }
     return;
@@ -92,4 +120,3 @@ observer.observe(document, {
   childList: true,
   subtree: true
 });
-
