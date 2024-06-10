84b207722e1468c405b640c94c058cf826464492
Lieven Vernaillen <lieven.vernaillen@smartphoto.com>
2024-06-10 21:16:09 +0200

    Restyle info and popup page

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Jira Copy to Clipboard
 
-A simple Chrome extension that allows "Copy to clipboard" functionality in Jira Cloud. Quickly copy the issue id + title to the clipboard. You can configure which fields exactly get copied.
+A simple Chrome extension that allows "Copy to clipboard" functionality in Jira Cloud. Quickly copy fields like issue id and title to the clipboard. You can configure which fields get copied and how it is formatted.
 
 Forked from [Jira-Snippet](https://github.com/daemonLibra/Jira-Snippet).
 
@@ -12,6 +12,26 @@ Forked from [Jira-Snippet](https://github.com/daemonLibra/Jira-Snippet).
 - Supports dark mode.
 
 ## Images
+- TODO
 
 ## Copy format examples
+- TODO
 
+## Installation
+
+This extension hasn't been published to any browser marketplace.
+But you can easily install it yourself.
+
+### Method 1: Load as unpacked extension from source code (Developer Mode)
+- Download the source files .zip from the [latest release](https://github.com/lvernaillen/jira-copy-to-clipboard/releases/latest).
+  - Extract the zip file to a location of your choice.
+- You can also clone the repo instead of downloading the .zip file.
+- Enable Developer Mode in your browser:
+  - Open Chrome.
+  - Navigate to the Extensions page by clicking the three vertical dots in the top-right corner, then `More tools` > `Extensions`, or directly visit [chrome://extensions/](chrome://extensions/).
+  - Toggle the Developer mode switch in the top-right corner of the Extensions page.
+- Click `Load unpacked` on the Extensions page.
+  - Navigate to the location where you unzipped the source code.
+
+### Method 2: Install from browser marketplace
+- TODO
--- scripts/options.js
+++ scripts/options.js
@@ -1,16 +1,49 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.addEventListener('click', function (ev) {
-    if (ev.target == document.getElementById('save')) {
-      const format = document.getElementById('format').value;
-      storageSet({ format: format }).then(function () {
-        alert('Format saved successfully.');
-      });
+    const saveButton = document.getElementById('save');
+    if (ev.target == saveButton) {
+      saveClipboardFormat(saveButton);
     }
   });
 
+  loadClipboardFormat();
+});
+
+function saveClipboardFormat(saveButton) {
+  const format = document.getElementById('format').value;
+  const saveText = saveButton.textContent;
+  storageSet({ format: format })
+  .then(function () {
+    saveButton.textContent = "Saved!";
+    saveButton.disabled = true
+    setTimeout(function () {
+      saveButton.textContent = saveText;
+      saveButton.disabled = false
+    }, 1000);
+  })
+  .catch((error) => {
+    console.error(`Save failed: ${error.message}`);
+    saveButton.textContent = "Save failed!";
+    saveButton.style.transition = 'none';
+    saveButton.classList.add('failed', 'no-hover');
+    saveButton.disabled = true;
+    setTimeout(function () {
+      saveButton.textContent = saveText;
+      saveButton.style.transition = '';
+      saveButton.classList.remove('failed', 'no-hover');
+      saveButton.disabled = false
+    }, 1000);
+  });
+}
+
+function loadClipboardFormat() {
   storageGet('format').then(function (data) {
     if (data.format) {
       document.getElementById('format').value = data.format;
     }
+
+    else {
+      document.getElementById('format').value = "{key}: {title}";
+    }
   });
-});
+}
